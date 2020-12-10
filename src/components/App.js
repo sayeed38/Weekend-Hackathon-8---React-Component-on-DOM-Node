a87e92e27b1f633fae1836d94797090fbb45ad3e
Mohammed Sayeed <34248417+sayeed38@users.noreply.github.com>
2020-12-11 01:00:51 +0530

    Fixed the DOM Node

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,10 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React from "react";
 
+export default function App() {
+  return (
+    <p>
+      Now I can render any React component on any DOM node I want using
+      ReactDOM.render
+    </p>
+  );
+}
